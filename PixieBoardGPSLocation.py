3be533dcac8e0a2dff84d065106ac4e68ad47466
Roberto Himmelbauer P <robertohimmelbauer@Robertos-MacBook-Pro.local>
2018-10-01 13:40:26 -0700

    Added parsing functions to GPSLocation class

--- PixieBoardGPSLocation.py
+++ PixieBoardGPSLocation.py
@@ -8,7 +8,9 @@ class PixieBoardGPSLocation():
 		self.PIXIE_BOARDS_PASSWORD = "pixiepro"
 		self.COMMAND_OK_CALLBACK = "OK"
 		self.ENABLE_AT_COMMAND = "echo 'ATE1' | socat - /dev/ttyUSB2,cr"
-		self.CONFIGURE_GPS_TRACKING = "echo 'AT+QGPS=1,30,50,0,1' | socat - /dev/ttyUSB2,cr | grep 'OK'"
+		self.SESSION_STATUS = "echo 'AT+QGPS?' | socat - /dev/ttyUSB2,cr | grep '+QGPS:'"
+		self.STOP_SESSION = "echo 'AT+QGPSEND' | socat - /dev/ttyUSB2,cr"
+		self.CONFIGURE_GPS_TRACKING = "echo 'AT+QGPS=1,30,50,0,1' | socat - /dev/ttyUSB2,cr"
 		self.GET_GPS_LOCATION = "echo 'AT+QGPSLOC?' | socat - /dev/ttyUSB2,cr"
 		self.GET_GPS_LOCATION_PRETTY = "echo 'AT+QGPSLOC=2' | socat - /dev/ttyUSB2,cr"
 
@@ -28,34 +30,52 @@ class PixieBoardGPSLocation():
 
 
 	def EnableATCommands(self):
-		command = subprocess.Popen([self.ENABLE_AT_COMMAND], stdout=subprocess.PIPE, shell=True)
-		(command_output, error) = command.communicate(self.PIXIE_BOARDS_PASSWORD)
-		if command_output == self.COMMAND_OK_CALLBACK:
+		(command_output, error) = self.SendShellCommand(self.ENABLE_AT_COMMAND)
+		if self.ParseOKInMsg(command_output):
 			return True, command_output, error
 		else:
 			return False, command_output, error
 
+	def StopSession(self):
+		if self.SessionStatus():
+			(command_output, error) = self.SendShellCommand(self.STOP_SESSION)
+			if self.ParseOKInMsg(command_output):
+				return True, command_output, error
+			else:
+				return False, command_output, error
+
+
+	def SessionStatus(self):
+		(command_output, error) = self.SendShellCommand(self.SESSION_STATUS)
+		if (str(command_output)[:-2] == "1":
+			return True
+		else:
+			return False
+
+
 	def ConfigureGPSTracking(self):
-		command = subprocess.Popen([self.CONFIGURE_GPS_TRACKING], stdout=subprocess.PIPE, shell=True)
-		(command_output, error) = command.communicate(self.PIXIE_BOARDS_PASSWORD)
-		if command_output == self.COMMAND_OK_CALLBACK:
+		(command_output, error) = self.SendShellCommand(self.CONFIGURE_GPS_TRACKING)
+		if self.ParseOKInMsg(command_output):
 			return True, command_output, error
 		else:
 			return False, command_output, error
 
 	def GetGPSLocation(self):
-		command = subprocess.Popen([self.GET_GPS_LOCATION], stdout=subprocess.PIPE, shell=True)
-		(command_output, error) = command.communicate(self.PIXIE_BOARDS_PASSWORD)
+		(command_output, error) = self.SendShellCommand(self.GET_GPS_LOCATION)
 		return command_output, error
 
 	def GetGPSLocationPretty(self):
-		command = subprocess.Popen([self.GET_GPS_LOCATION_PRETTY], stdout=subprocess.PIPE, shell=True)
+		(command_output, error) = self.SendShellCommand(self.GET_GPS_LOCATION_PRETTY)
+		return command_output, error
+
+	def SendShellCommand(self, shellCommand):
+		command = subprocess.Popen([shellCommand], stdout=subprocess.PIPE, shell=True)
 		(command_output, error) = command.communicate(self.PIXIE_BOARDS_PASSWORD)
 		return command_output, error
 
 	def ParseGPSLocation(self, command_output):
-		locationData = command_output.split(",")
-		self.UTCTime = locationData[0][9:]
+		locationData = str(command_output).split(",")
+		self.UTCTime = locationData[0][30:]
 		self.Latitude = locationData[1]
 		self.Longitude = locationData[2]
 		self.HorizontalPrecision = locationData[3]
@@ -65,6 +85,13 @@ class PixieBoardGPSLocation():
 		self.SpeedOverGroundKmH = locationData[7]
 		self.SpeedOverGroundKnots = locationData[8]
 		self.Date = locationData[9]
-		self.NumberOfSatellites = locationData[10]
+		self.NumberOfSatellites = locationData[10][0:2]
+
+	def ParseOKInMsg(self, command_output):
+		output = str(command_output)
+		if COMMAND_OK_CALLBACK in output:
+			return True
+		else:
+			return False
 
 
