75c970473425b4d3751f9b1154cee71fe6de9cbf
Roberto Himmelbauer P <robertohimmelbauer@Robertos-MacBook-Pro.local>
2018-10-01 12:08:48 -0700

    Added self on functions in class file

--- PixieBoardGPSLocation.py
+++ PixieBoardGPSLocation.py
@@ -2,17 +2,17 @@ import subprocess
 
 
 class PixieBoardGPSLocation():
-	PIXIE_BOARDS_PASSWORD = "pixiepro"
+	self.PIXIE_BOARDS_PASSWORD = "pixiepro"
 
-	COMMAND_OK_CALLBACK = "OK"
+	self.COMMAND_OK_CALLBACK = "OK"
 
-	ENABLE_AT_COMMAND = "echo 'ATE1' | socat - /dev/ttyUSB2,cr | grep 'OK'"
+	self.ENABLE_AT_COMMAND = "echo 'ATE1' | socat - /dev/ttyUSB2,cr | grep 'OK'"
 
-	CONFIGURE_GPS_TRACKING = "echo 'AT+QGPS=1,30,50,0,1' | socat - /dev/ttyUSB2,cr | grep 'OK'"
+	self.CONFIGURE_GPS_TRACKING = "echo 'AT+QGPS=1,30,50,0,1' | socat - /dev/ttyUSB2,cr | grep 'OK'"
 
-	GET_GPS_LOCATION = "echo 'AT+QGPSLOC?' | socat - /dev/ttyUSB2,cr"
+	self.GET_GPS_LOCATION = "echo 'AT+QGPSLOC?' | socat - /dev/ttyUSB2,cr"
 
-	GET_GPS_LOCATION_PRETTY = "echo 'AT+QGPSLOC=2' | socat - /dev/ttyUSB2,cr"
+	self.GET_GPS_LOCATION_PRETTY = "echo 'AT+QGPSLOC=2' | socat - /dev/ttyUSB2,cr"
 
 	def __init__(self):
 		self.ModemStatus = ""
@@ -29,31 +29,31 @@ class PixieBoardGPSLocation():
 		self.Date = ""
 		self.NumberOfSatellites = ""
 
-	def EnableATCommands():
-		command = subprocess.Popen([ENABLE_AT_COMMAND], stdout=subprocess.PIPE, shell=True)
-		(command_output, error) = command.communicate(PIXIE_BOARDS_PASSWORD)
-		if command_output == COMMAND_OK_CALLBACK:
+	def EnableATCommands(self):
+		command = subprocess.Popen([self.ENABLE_AT_COMMAND], stdout=subprocess.PIPE, shell=True)
+		(command_output, error) = command.communicate(self.PIXIE_BOARDS_PASSWORD)
+		if command_output == self.COMMAND_OK_CALLBACK:
 			return True, command_output, error
 		else:
 			return False, command_output, error
 
-	def ConfigureGPSTracking():
-		command = subprocess.Popen([CONFIGURE_GPS_TRACKING], stdout=subprocess.PIPE, shell=True)
-		(command_output, error) = command.communicate(PIXIE_BOARDS_PASSWORD)
-		if command_output == COMMAND_OK_CALLBACK:
+	def ConfigureGPSTracking(self):
+		command = subprocess.Popen([self.CONFIGURE_GPS_TRACKING], stdout=subprocess.PIPE, shell=True)
+		(command_output, error) = command.communicate(self.PIXIE_BOARDS_PASSWORD)
+		if command_output == self.COMMAND_OK_CALLBACK:
 			return True
 		else:
 			return False
 
-	def GetGPSLocation():
-		command = subprocess.Popen([GET_GPS_LOCATION], stdout=subprocess.PIPE, shell=True)
-		(command_output, error) = command.communicate(PIXIE_BOARDS_PASSWORD)
+	def GetGPSLocation(self):
+		command = subprocess.Popen([self.GET_GPS_LOCATION], stdout=subprocess.PIPE, shell=True)
+		(command_output, error) = command.communicate(self.PIXIE_BOARDS_PASSWORD)
 
-	def GetGPSLocationPretty():
-		command = subprocess.Popen([GET_GPS_LOCATION_PRETTY], stdout=subprocess.PIPE, shell=True)
-		(command_output, error) = command.communicate(PIXIE_BOARDS_PASSWORD)
+	def GetGPSLocationPretty(self):
+		command = subprocess.Popen([self.GET_GPS_LOCATION_PRETTY], stdout=subprocess.PIPE, shell=True)
+		(command_output, error) = command.communicate(self.PIXIE_BOARDS_PASSWORD)
 
-	def ParseGPSLocation(command_output):
+	def ParseGPSLocation(self, command_output):
 		locationData = command_output.split(",")
 		self.UTCTime = locationData[0][9:]
 		self.Latitude = locationData[1]
