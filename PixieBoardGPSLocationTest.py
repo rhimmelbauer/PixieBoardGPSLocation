f7d5533ea74143f5192f0e61caf827d2a7684fd7
Roberto Himmelbauer <roberto.himmelbauer@codeandmodules.com>
2018-10-01 15:03:39 -0700

    Finished testing the testing script

--- PixieBoardGPSLocationTest.py
+++ PixieBoardGPSLocationTest.py
@@ -5,14 +5,14 @@ def TestEnableATCommands():
 	isEnabled, raw, error = pxbdGPSLocation.EnableATCommands()
 	print("Enabled: {}\n Output: {}\n Error: {}".format(isEnabled, raw, error))
 	
-def TestSessionStatus(self):
+def TestSessionStatus():
 	pxbdGPSLocation = PixieBoardGPSLocation()
 	status, raw, error = pxbdGPSLocation.SessionStatus()
 	print("Session Status: {}\n Output: {}\n Error: {}".format(status, raw, error))
 
 def TestStopSession():
 	pxbdGPSLocation = PixieBoardGPSLocation()
-	sessionStoped, raw, error = pxbdGPSLocation.SessionStatus()
+	sessionStoped, raw, error = pxbdGPSLocation.StopSession()
 	print("Stopped: {}\n Output: {}\n Error: {}".format(sessionStoped, raw, error))
 
 def TestConfigureGPSLocation():
@@ -24,6 +24,18 @@ def TestGetGPSLocation():
 	pxbdGPSLocation = PixieBoardGPSLocation()
 	gotLocation, raw, error = pxbdGPSLocation.GetGPSLocation()
 	print("Stopped: {}\n Output: {}\n Error: {}".format(gotLocation, raw, error))
+	print("UTC Time: {}\n" \
+                "Latitude: {}\n" \
+                "Longitude: {}\n" \
+                "HorizontalPrecision: {}\n" \
+                "Altitude: {}\n" \
+                "Fix: {}\n" \
+                "Cog: {}\n" \
+                "SpeedOverGroundKmH: {}\n" \
+                "SpeedOverGroundKnots: {}\n" \
+                "Date: {}\n" \
+                "Number Of Satellites: {}".format(pxbdGPSLocation.UTCTime, pxbdGPSLocation.Latitude,  pxbdGPSLocation.Longitude, pxbdGPSLocation.HorizontalPrecision, pxbdGPSLocation.Altitude, pxbdGPSLocation.Fix, pxbdGPSLocation.Cog, pxbdGPSLocation.SpeedOverGroundKmH, pxbdGPSLocation.SpeedOverGroundKnots, pxbdGPSLocation.Date, pxbdGPSLocation.NumberOfSatellites)
+	)
 
 def TestGetGPSLocationPretty():
 	pxbdGPSLocation = PixieBoardGPSLocation()
